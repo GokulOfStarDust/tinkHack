e63a867419902de92539efd65856a727ea5ac8e8
stardust <gokulgopalan18@gmail.com>
2025-03-23 00:02:43 +0530

    error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ "compilerOptions": {
+    "moduleResolution": "node"
+  }
 };
 
 export default nextConfig;
--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,5 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";  // This should exist
+
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
+}
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
